5673589dd8da0bd85c466c7e535e47254264d80c
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-09-12 15:50:03 -0400

    additional quasibehaviour work

--- Assets/Scripts/QuasiBehaviour.cs
+++ Assets/Scripts/QuasiBehaviour.cs
@@ -6,6 +6,8 @@ public abstract class QuasiBehaviour : MonoBehaviour
 {
     float QuasiTimeStep = 0.6f;
 
+    new public static QuasiBehaviour quasiBehaviour;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +21,21 @@ public abstract class QuasiBehaviour : MonoBehaviour
         QuasiUpdate();
         yield return new WaitForSecondsRealtime(QuasiTimeStep * Time.timeScale);
         QuasiLateUpdate();
+        // run the time again
+        StartCoroutine(QuasiTime());
+    }
+
+    // the coroutine that manages our fixed quasitime
+    public IEnumerator FixedQuasiTime()
+    {
+        QuasiFixedUpdate();
+        yield return new WaitForSecondsRealtime(QuasiTimeStep);
     }
 
     // our quasi behaviours
     public virtual void QuasiStart() { } // runs as the start for all quasi objects
     public virtual void QuasiUpdate() { } // runs as the update for all quasi objects
     public virtual void QuasiLateUpdate() { } // runs as the update for all quasi objects
+    public virtual void QuasiFixedUpdate() { } // runs as the update for all quasi objects
 
 }
